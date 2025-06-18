ab4885253e59fe6f143e589cffe5d944ff3054e6
chi <chi@taiwan.im>
2025-06-18 17:52:07 +0700

    feat: 版面調整

--- components/BoardGameAgricolaFence.vue
+++ components/BoardGameAgricolaFence.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
 import { ref, watch } from "vue";
 import { renderBoard } from "board-game-agricola-fence";
+const { fenceArray = [] } = defineProps<{ fenceArray?: string[] }>();
 const boardGameAgricolaFence = ref<HTMLElement>();
 const fenceSet = ref<Set<string>>(new Set());
 const closureSetArray = ref<Set<string>[]>([]);
 watch(boardGameAgricolaFence, () => {
   if (!boardGameAgricolaFence.value) return;
 
-  const { watch: boardWatch } = renderBoard(boardGameAgricolaFence.value);
+  const { fenceKeySet, watch: boardWatch } = renderBoard(boardGameAgricolaFence.value);
+  fenceKeySet.clear();
+  fenceArray.map((key) => fenceKeySet.add(key));
   boardWatch((a, b) => {
     fenceSet.value = a;
     closureSetArray.value = b || [];
@@ -15,6 +18,9 @@ watch(boardGameAgricolaFence, () => {
 });
 </script>
 <template>
+  <div class="bg-[#1099bb] p-1 max-w-300px">
+    <div ref="boardGameAgricolaFence" />
+  </div>
   <div class="flex">
     <div>柵欄數量:{{ fenceSet.size }}</div>
     <div>圈地數量:{{ closureSetArray.length }}</div>
@@ -23,8 +29,5 @@ watch(boardGameAgricolaFence, () => {
     <div v-for="(item, index) in closureSetArray">
       第{{ index + 1 }}圈地有{{ item.size }}格空間 = {{ item.size * 2 }} 動物
     </div>
-  </div>
-  <div class="bg-[#1099bb] p-1 max-w-300px">
-    <div ref="boardGameAgricolaFence" />
   </div>
 </template>
--- docs/article/board-game-agricola-fence.md
+++ docs/article/board-game-agricola-fence.md
@@ -1,5 +1,5 @@
 ---
-title: 農家樂圈地容量計算
+title: 桌遊農家樂圈地容量計算
 createtime: 202506181554
 ---
 
@@ -12,15 +12,32 @@ createtime: 202506181554
 3. 使用 pixi 及 pixi 生態
 4. vite 內使用 vue
 
-## 我用了套件
+## 我用的套件
 
 1. pixi.js
 2. pixi layout
 3. pixi react
 <script setup>
 import BoardGameAgricolaFence from '@/components/BoardGameAgricolaFence.vue';
+const fenceArray=[
+  "1-2-H",
+  "0-0-H",
+  "1-0-H",
+  "0-0-V",
+  "2-0-H",
+  "3-0-V",
+  "3-1-V",
+  "2-2-H",
+  "0-2-H",
+  "0-1-V",
+  "0-2-V",
+  "0-3-H",
+  "1-3-H",
+  "2-3-H",
+  "3-2-V"
+];
 </script>
 
 ## 程式展示 1.4.3 version
 
-<BoardGameAgricolaFence />
+<BoardGameAgricolaFence :fenceArray />
